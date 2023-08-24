5de3b6ee431d083ff17a6ca9d725f47568af3879
akari nari <lightakari7@gmail.com>
2023-08-24 18:00:31 +0000

    Norman draft

---
+++ norman.py
@@ -0,0 +1,12 @@
+restaurants = ["Din Tai Fung","Houston's","Paulette's Pupusas","Leah's Thai","Taco Bell"]
+new_restaurant = input("What restaurant would you like to add to the list?")
+ 
+preferred = ""
+ArithmeticError
+for i in restaurants:
+    print("A",restaurants[i], "B",restaurants[i+1])
+    if input("Do you prefer A or B") == "A":
+        print("A")
+
+        
+
